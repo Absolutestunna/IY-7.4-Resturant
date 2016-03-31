fce73b1d0543bc991455d910cf714ae1c852d886
Joshua Abu <joshujah007@gmail.com>
2016-03-31 19:28:22 -0400

    order sides working

--- app/components/welcome.jsx
+++ app/components/welcome.jsx
@@ -3,7 +3,7 @@ var Backbone = require('backbone');
 var ReactDOM = require('react-dom');
 var $ = require('jquery');
 require('backbone-react-component');
-// 
+//
 // <video id="welcomeVid" loop autoPlay>
 //   <source src="http://s3-eu-west-1.amazonaws.com/eu-west-1.vimeo.com/videos/452/085/452085124.mp4?AWSAccessKeyId=AKIAJXCO5OTU3FAVAVHA&Expires=1458532573&Signature=ITtghDg%2BnZx%2F7wj2WR3sLGyhjkk%3D" type="video/mp4" />
 // </video>
@@ -20,10 +20,9 @@ var WelcomePageComponent = React.createClass({
     return (
       <div className="row">
         <div className="col-md-12">
-          //video here
         <div className='overlay'>
           <div className='overlay-content'>
-            <p className='first'>MAD CITY'S</p>
+            <p className='first'>MAD CITYS</p>
             <p className='second'>RECIPIES</p>
             <div className="welcome" onClick={this.handleWebsiteAccess}><a href="#">Welcome</a><i className="icon-chevron-right"></i>
             </div>
